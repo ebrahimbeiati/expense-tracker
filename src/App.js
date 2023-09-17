089ed606b1cccb33a3ce6da10302f8f0f0ce45d0
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-09-17 18:00:37 +0100

    Added GlobalProvider to App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Balance from './components/Balance';
  import IncomeExpenses from './components/IncomeExpenses'
  import TransactionList from './components/TransactionList';
  import AddTransaction from './components/AddTransaction';
+ import { GlobalProvider } from './context/GlobalState';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <GlobalProvider className="App">
    <Header />
    <div className="container">
       <Balance />
@@ -17,7 +18,7 @@ function App() {
       <AddTransaction />
    </div>
   
-    </div>
+    </GlobalProvider>
   );
 }
 
---
+++ src/context/GlobalState.js
@@ -0,0 +1,41 @@
+import React, {createContext,useReducer} from 'react'
+import { AppReducer } from './AppReducer'
+
+const initialState ={
+    transactions:[
+        {id:1,text:'Flower',amount:-20},
+        {id:2,text:'Salary',amount:+300},
+        {id:3,text:'Book',amount:-10},
+        {id:4,text:'Camera',amount:+150}
+    ]
+}
+//reducer function is a pure function that takes the current state and an action as arguments. It returns new state based on these two parameters
+const reducer = (state,action)=>{
+    switch(action.type){
+        case 'DELETE_TRANSACTION':
+            return{
+                ...state,
+                transactions:state.transactions.filter(transaction=>transaction.id !== action.payload)
+            }
+        case 'ADD_TRANSACTION':
+            return{
+                ...state,
+                transactions:[...state.transactions,action.payload]
+            }
+        default:
+            return state
+    }
+}
+export const GlobalContext = createContext(initialState)
+// export const useGlobalState=()=>{
+//     return useContext(GlobalContext)
+// }
+//Provider component
+export const GlobalProvider = ({children})=>{
+    const [state,dispatch] = useReducer(reducer,initialState)
+    return(
+        <GlobalContext.Provider value={{transactions:state.transactions,dispatch}}>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
