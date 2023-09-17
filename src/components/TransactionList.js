2a5bfff2488a3a85f0c08a54ca2364b999e3372d
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-09-17 19:32:34 +0100

    Update TransactionList component to use the transaction object instead of the text property.

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -7,14 +7,14 @@ const TransactionList = () => {
     const { transactions } = useContext(GlobalContext);
    
   return (
-    <div>
+    <>
       <h2>Transaction History</h2>
         <ul id='list' className='list'>
-        {transactions.map(transaction =>(<Transaction key={transaction.id} transaction={transaction.text}
+        {transactions.map(transaction =>(<Transaction key={transaction.id} transaction={transaction}
         amount={transaction.amount} />
        ))}
         </ul>
-    </div>
+    </>
   )
 }
 
