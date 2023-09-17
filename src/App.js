df7b284e9b224066f8ce3b9651b2012ad765d16e
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-09-17 17:09:57 +0100

    Added Balance and IncomeExpenses components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Header from './components/Header';
+import Balance from './components/Balance';
+ import IncomeExpenses from './components/IncomeExpenses'
+// import Transactions from './components/Transactions'
+// import AddTransaction from './components/AddTransaction'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
    <Header />
+   <div className="container">
+      <Balance />
+      <IncomeExpenses />
+   </div>
   
     </div>
   );
